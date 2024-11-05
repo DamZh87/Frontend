8ce67dcc8a68620f8c629e6971109599b924ee85
DamZh87 <xiaomidamir@gmail.com>
2024-11-05 22:50:44 +0500

    05.10.24

---
+++ Lessons/Online/05.11.2024/main.js
@@ -0,0 +1,34 @@
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then((response) => {
+//     console.log(response);
+//     if (response.status === 404){
+//         PromiseRejectionEvent()
+//     }
+
+//     response.json()
+//   })
+//   .then((json) => {
+//     console.log(json)
+//     console.log('All Ok');
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//     console.error('I error from catch');
+//   })
+//   .finally(() => {
+//     console.log('I finaly');
+
+//   })
+
+
+
+//   async function getUsers() {
+//    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//  console.log(response);
+ 
+
+//   }
+
+//   getUsers();
