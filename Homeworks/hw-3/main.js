1059fc01c5e75afa24c92558c75c03da0d9932e1
DamZh87 <xiaomidamir@gmail.com>
2024-11-23 16:01:37 +0500

    777

--- Homeworks/hw-3/main.js
+++ Homeworks/hw-3/main.js
@@ -7,137 +7,132 @@ useShopItems();
 
 
 
-filterSelect.addEventListener('input', (e)=>{
-  const {value} = e.target;
+filterSelect.addEventListener ('input', (e) => {
+  const { value } = e.target;
   const initialItems = [...shopItems];
-  
-  if (value === 'onTitle') {
+  const checked = poorItemsRemoveCheckbox.checked; 
+  const filteredPoorItems = [...shopItems].filter((catalogItem)=> catalogItem.price >= 500)
 
-  const sortedOnTitle = initialItems.sort((a, b)=> a.title.toLowerCase().localeCompare(b.title.toLocaleLowerCase()))
-    
+  if (value === 'onTitle' && checked) {
+    const sortedOnTitle = filteredPoorItems.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()));
+    return getCatalog(sortedOnTitle);
+  }
+  if (value === 'onTitle' && !checked) {
+    const sortedOnTitle = initialItems.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()));
     return getCatalog(sortedOnTitle);
   }
 
-  if (value === 'poor') {
+  if (value === 'poor' && checked) {
+    const sortedPoor = filteredPoorItems.sort((a, b)=> a.price > b.price ? 1 : -1)
+    return getCatalog(sortedPoor);
+  }
+
+  if (value === 'poor' && !checked) {
     const sortedPoor = initialItems.sort((a, b)=> a.price > b.price ? 1 : -1)
     return getCatalog(sortedPoor);
   }
   
-  if (value === 'rich') {
+  if (value === 'rich' && checked ) {
+    const sortedRich = filteredPoorItems.sort((a, b)=> a.price < b.price ? 1 : -1)
+    return getCatalog(sortedRich);
+  }
+
+  if (value === 'rich' && !checked) {
     const sortedRich = initialItems.sort((a, b)=> a.price < b.price ? 1 : -1)
     return getCatalog(sortedRich);
   }
 
-  if (value === 'storageMin') {
+  if (value === 'storageMin' && checked) {
+    const sortedStorageMin = filteredPoorItems.sort((a, b)=> a.quantity > b.quantity ? 1 : -1)
+    return getCatalog(sortedStorageMin);
+  }
+  if (value === 'storageMin' && !checked) {
     const sortedStorageMin = initialItems.sort((a, b)=> a.quantity > b.quantity ? 1 : -1)
     return getCatalog(sortedStorageMin);
   }
   
-  if (value === 'storageMax') {
+  if (value === 'storageMax' && checked ) {
+    const sortedStorageMax = filteredPoorItems.sort((a, b)=> a.quantity < b.quantity ? 1 : -1)
+    return getCatalog(sortedStorageMax);
+  }
+
+  if (value === 'storageMax' && !checked) {
     const sortedStorageMax = initialItems.sort((a, b)=> a.quantity < b.quantity ? 1 : -1)
     return getCatalog(sortedStorageMax);
   }
-  if (value === 'default') {
+
+  if (value === 'default' && checked) {
+    getCatalog(filteredPoorItems);
+  }
+  if (value === 'default' && !checked) {
     getCatalog(shopItems);
   }
-  
-  
+ 
 })
-
 
 
 poorItemsRemoveCheckbox.addEventListener('input', (e)=>{
-  const {checked} = e.target;
-
-  if (checked) {
-    const filteredPoorItems = [...shopItems].filter((catalogItem)=> catalogItem.price >= 500)
-    getCatalog(filteredPoorItems)
-   
-    filterSelect.addEventListener('input', (e)=>{
-      const {value} = e.target;
-      const initialItems = [...filteredPoorItems];
-      
-      if (value === 'onTitle') {
-    
-      const sortedOnTitle = initialItems.sort((a, b)=> a.title.toLowerCase().localeCompare(b.title.toLocaleLowerCase()))
-        
-        return getCatalog(sortedOnTitle);
-      }
-    
-      if (value === 'poor') {
-        const sortedPoor = initialItems.sort((a, b)=> a.price > b.price ? 1 : -1)
-        return getCatalog(sortedPoor);
-      }
-      
-      if (value === 'rich') {
-        const sortedRich = initialItems.sort((a, b)=> a.price < b.price ? 1 : -1)
-        return getCatalog(sortedRich);
-      }
-    
-      if (value === 'storageMin') {
-        const sortedStorageMin = initialItems.sort((a, b)=> a.quantity > b.quantity ? 1 : -1)
-        return getCatalog(sortedStorageMin);
-      }
-      
-      if (value === 'storageMax') {
-        const sortedStorageMax = initialItems.sort((a, b)=> a.quantity < b.quantity ? 1 : -1)
-        return getCatalog(sortedStorageMax);
-      }
-      if (value === 'default') {
-        getCatalog(filteredPoorItems);
-      }
-      
-     
-    })
-    
-  } else {
-    
-   
-    
-filterSelect.addEventListener('input', (e)=>{
-  const {value} = e.target;
-  const initialItems = [...shopItems];
   
-  if (value === 'onTitle') {
-
-  const sortedOnTitle = initialItems.sort((a, b)=> a.title.toLowerCase().localeCompare(b.title.toLocaleLowerCase()))
-    
+  const initialItems = [...shopItems];
+  const {checked} = e.target; 
+  const filteredPoorItems = [...shopItems].filter((catalogItem)=> catalogItem.price >= 500)
+  const value = filterSelect.value;
+  
+  if (value === 'onTitle' && checked) {
+    const sortedOnTitle = filteredPoorItems.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()));
     return getCatalog(sortedOnTitle);
   }
+  if (value === 'onTitle' && !checked) {
+    const sortedOnTitle = initialItems.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()));
+    return getCatalog(sortedOnTitle);
+  }
+  if (value === 'poor' && checked) {
+    const sortedPoor = filteredPoorItems.sort((a, b)=> a.price > b.price ? 1 : -1)
+    return getCatalog(sortedPoor);
+  }
 
-  if (value === 'poor') {
+  if (value === 'poor' && !checked) {
     const sortedPoor = initialItems.sort((a, b)=> a.price > b.price ? 1 : -1)
     return getCatalog(sortedPoor);
   }
   
-  if (value === 'rich') {
+  if (value === 'rich' && checked ) {
+    const sortedRich = filteredPoorItems.sort((a, b)=> a.price < b.price ? 1 : -1)
+    return getCatalog(sortedRich);
+  }
+
+  if (value === 'rich' && !checked) {
     const sortedRich = initialItems.sort((a, b)=> a.price < b.price ? 1 : -1)
     return getCatalog(sortedRich);
   }
 
-  if (value === 'storageMin') {
+  if (value === 'storageMin' && checked) {
+    const sortedStorageMin = filteredPoorItems.sort((a, b)=> a.quantity > b.quantity ? 1 : -1)
+    return getCatalog(sortedStorageMin);
+  }
+  if (value === 'storageMin' && !checked) {
     const sortedStorageMin = initialItems.sort((a, b)=> a.quantity > b.quantity ? 1 : -1)
     return getCatalog(sortedStorageMin);
   }
   
-  if (value === 'storageMax') {
+  if (value === 'storageMax' && checked ) {
+    const sortedStorageMax = filteredPoorItems.sort((a, b)=> a.quantity < b.quantity ? 1 : -1)
+    return getCatalog(sortedStorageMax);
+  }
+
+  if (value === 'storageMax' && !checked) {
     const sortedStorageMax = initialItems.sort((a, b)=> a.quantity < b.quantity ? 1 : -1)
     return getCatalog(sortedStorageMax);
   }
-  if (value === 'default') {
+
+  if (value === 'default' && checked) {
+    getCatalog(filteredPoorItems);
+  }
+  if (value === 'default' && !checked) {
     getCatalog(shopItems);
   }
+
 })
-
-getCatalog(shopItems);
-
-
-  }
-  
-})
-
-
-
 
 });
 
