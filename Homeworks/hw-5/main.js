e0c09220b7c217678e5d3e85087797a6ecc25e4f
DamZh87 <xiaomidamir@gmail.com>
2024-11-29 23:59:14 +0500

    123

--- Homeworks/hw-5/main.js
+++ Homeworks/hw-5/main.js
@@ -1,55 +1,78 @@
 class Request {
-    constructor(baseUrl) {
-      this.baseUrl = baseUrl;
-    }
-      async get(endpoint) {
-      return this._fetch(endpoint, {
-        method: 'GET'
-      });
-    }
-   
-      async _fetch(endpoint) {
-      try {
-        const response = await fetch(`${this.baseUrl}${endpoint}`);
-  
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return await response.json();
-      } catch (error) {
-        console.error('Fetch error:', error);
-        throw error;
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+    async get(endpoint) {
+    return this._fetch(endpoint, {
+      method: 'GET'
+    });
+  }
+ 
+    async _fetch(endpoint) {
+    try {
+      const response = await fetch(`${this.baseUrl}${endpoint}`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
+      return await response.json();
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw error;
     }
-  };
+  }
+}
+
+const request = new Request('https://jsonplaceholder.typicode.com');
+(async () => {
+try {
+ const responseGet = await request.get('/users');
+ getUserList(responseGet); 
+  } catch (error) {
+  console.error('Request error:', error);
+}
+})()
+
+
+
 
 const listEl = document.querySelector('.list');
 function renderUserCard({
     name = '',
-    username = '',
+    website = '',
     email	 = '',
     address = '',
+    company = '',
+    
 }) 
     {
-    const card = document.createElement('article');
+    const card = document.createElement('div');
     const nameEl = document.createElement('h3');
-    const usernameEl = document.createElement('h3');
-    const emailEl = document.createElement('h3');
-    const addressEl = document.createElement('h3');
-    
+    const cityEl = document.createElement('p');
+    const company_nameEl = document.createElement('middle');
+    const emailEl = document.createElement('a');
+    const siteEl = document.createElement('a');
     
     card.className = 'user__card'
-  
-  
+    nameEl.className = 'user__name'
+    cityEl.className ='city_name'
+    company_nameEl.className ='company_name'
+    emailEl.className ='p_small'
+    siteEl.className ='p_small'
+
     nameEl.textContent = name;
-    usernameEl.textContent = `Имя пользователя: ${username}`;
-    emailEl.textContent = email;
-    addressEl.textContent = JSON.stringify(address.city);
-     
+    siteEl.setAttribute('href',`www.`+ website);
+    siteEl.setAttribute('target', '_blank');
+    siteEl.textContent = `Visit website: ${website}`;
+    emailEl.setAttribute('href',`mailto:`+email);
+    emailEl.textContent = `Mail to: ${email}`;
+    cityEl.textContent = `From ${JSON.stringify(address.city)}`;
+    company_nameEl.textContent = `Works in: ${JSON.stringify(company.name)} company`
     card.appendChild(nameEl)
-    card.appendChild(usernameEl)
+    card.appendChild(cityEl)
+    card.appendChild(company_nameEl)
     card.appendChild(emailEl)
-    card.appendChild(addressEl)
+    card.appendChild(siteEl)
 
     return card;
   }
@@ -62,18 +85,8 @@ function renderUserCard({
     });
   }
 
-  const request = new Request('https://jsonplaceholder.typicode.com');
-  (async () => {
-  try {
-   const responseGet = await request.get('/users');
-   getUserList(responseGet);
-   console.log(responseGet);
-   
-    
-    } catch (error) {
-    console.error('Request error:', error);
-  }
-})()
+
+ 
 
 
 
