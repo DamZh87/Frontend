aa8de5894d100da7c25ad7d2bf3d853e65567ab6
DamZh87 <xiaomidamir@gmail.com>
2025-01-03 22:22:38 +0500

    My-app

---
+++ 03.01.2025/my-app/src/components/PasswordInput.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Button from "./Button"
+import styles from './paswordinput.module.css';
+
+const PasswordInput = () => {
+    const [isShow, setIsShow] = useState(false);
+  
+const handleToogleShow = ()=>{
+    if (isShow) {
+      setIsShow(false);
+       } else{
+        setIsShow(true);
+    }
+  }
+  return(
+    <div className={styles.container}>
+        <input className={styles.pass_input} type={isShow? "text" : "password"} >
+            </input> 
+
+        <Button
+            onClick={handleToogleShow} 
+            
+        >
+            {isShow? 'Скрыть пароль' : 'Показать пароль'}
+        </Button>
+
+    </div>
+    )
+  }
+
+  export default PasswordInput;
--- Homeworks/hw-6/speak.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.sp_btn').addEventListener('click', () => {
-    let utterance = new SpeechSynthesisUtterance(document.querySelector('.text').value);
-    utterance.lang = 'ru-RU';  
-    speechSynthesis.speak(utterance); 
-})
